53a1b7bd72fcecb956c8770b3b7a5e79db15bf6f
edinalami <edinalami@gmail.com>
2021-03-27 23:41:54 +0100

    Add interactor and datasource packages

---
+++ app/src/main/java/hu/bme/aut/news4you/database/DiskDataSource.kt
@@ -0,0 +1,8 @@
+package hu.bme.aut.news4you.database
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class DiskDataSource @Inject constructor(
+)
---
+++ app/src/main/java/hu/bme/aut/news4you/network/NetworkDataSource.kt
@@ -0,0 +1,8 @@
+package hu.bme.aut.news4you.network
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class NetworkDataSource @Inject constructor(
+)
